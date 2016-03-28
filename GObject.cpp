d74761a5268cee17fe4668241abdec6e9e61e6e9
Gediminas Jancys <g.jancys@gmail.com>
2016-03-28 16:26:55 +0300

    Sukurtas GObject klase

---
+++ GObject.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   GObject.cpp
+ * Author: gedas
+ * 
+ * Created on Pirmadienis, 2016, kovas 28, 16.22
+ */
+
+#include "GObject.h"
+
+GObject::GObject() {
+}
+
+GObject::~GObject() {
+}
+
+std::string GObject::getClassName(){
+    return this->className;
+}
+
---
+++ GObject.h
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   GObject.h
+ * Author: gedas
+ *
+ * Created on Pirmadienis, 2016, kovas 28, 16.22
+ */
+
+#ifndef GOBJECT_H
+#define GOBJECT_H
+
+class GObject {
+public:
+    // ##### Kintamieji #####
+    // ##### END Kintamieji #####
+    // #########################################################################
+    // ##### Metodai #####
+    GObject();
+    virtual ~GObject();
+    
+    /* getClassName
+     * Metodas skirtas grazinti klases padavinimui */
+    std::string getClassName();
+    // ##### END Metodai #####
+private:
+    // ##### Kintamieji #####
+    // ##### END Kintamieji #####
+    // #########################################################################
+    // ##### Metodai #####
+    /* className
+     * Kintamasis skirtas saugoti klases pavadinimui */
+    std::string className;
+    // ##### END Metodai #####
+};
+
+#endif /* GOBJECT_H */
+
