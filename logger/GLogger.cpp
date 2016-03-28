8a633f8e1d4d29e62c2bce27e9ab6ae8b3183d20
Gediminas Jancys <g.jancys@gmail.com>
2016-03-28 16:46:26 +0300

    Sukurtas GLogger

---
+++ logger/GLogger.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   GLogger.cpp
+ * Author: gedas
+ * 
+ * Created on Pirmadienis, 2016, kovas 28, 16.44
+ */
+
+#include "GLogger.h"
+
+GLogger::GLogger() {
+}
+
+GLogger::GLogger(const GLogger& orig) {
+}
+
+GLogger::~GLogger() {
+}
+
---
+++ logger/GLogger.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   GLogger.h
+ * Author: gedas
+ *
+ * Created on Pirmadienis, 2016, kovas 28, 16.44
+ */
+
+#ifndef GLOGGER_H
+#define GLOGGER_H
+
+class GLogger : public GObject {
+public:
+    // ##### Kintamieji #####
+    // ##### END Kintamieji #####
+    // #########################################################################
+    // ##### Metodai #####
+    GLogger();
+    virtual ~GLogger();
+    // ##### END Metodai #####
+private:
+    // ##### Kintamieji #####
+    // ##### END Kintamieji #####
+    // #########################################################################
+    // ##### Metodai #####
+    // ##### END Metodai #####
+};
+
+#endif /* GLOGGER_H */
+
